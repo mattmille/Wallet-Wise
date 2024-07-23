971ee1c347f5f0b3671dcd202ae5e4e8d0c9f7fe
mattmille <matthewm722@gmail.com>
2024-07-22 21:04:38 -0400

    Think I fixed api

--- go.mod
+++ go.mod
@@ -7,16 +7,16 @@ require (
 	github.com/gin-gonic/contrib v0.0.0-20240508051311-c1c6bf0061b0
 	github.com/gin-gonic/gin v1.10.0
 	github.com/mattn/go-sqlite3 v1.14.14
+	github.com/pkg/errors v0.9.1
+	github.com/spf13/viper v1.12.0
 	github.com/volatiletech/null/v8 v8.1.2
+	github.com/volatiletech/randomize v0.0.1
 	github.com/volatiletech/sqlboiler/v4 v4.16.2
 	github.com/volatiletech/strmangle v0.0.6
 	modernc.org/sqlite v1.18.1
 )
 
 require (
-	github.com/Masterminds/goutils v1.1.1 // indirect
-	github.com/Masterminds/semver/v3 v3.1.1 // indirect
-	github.com/Masterminds/sprig/v3 v3.2.2 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
@@ -31,36 +31,26 @@ require (
 	github.com/gofrs/uuid v4.2.0+incompatible // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/huandu/xstrings v1.3.2 // indirect
-	github.com/imdario/mergo v0.3.13 // indirect
-	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
-	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/spf13/afero v1.9.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
-	github.com/spf13/cobra v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.12.0 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/volatiletech/inflect v0.0.1 // indirect
-	github.com/volatiletech/randomize v0.0.1 // indirect
-	github.com/volatiletech/sqlboiler v3.7.1+incompatible // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/mod v0.8.0 // indirect
--- main.go
+++ main.go
@@ -1,30 +1,140 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"log"
-	"wallet-wise/routes"
+	"net/http"
+	"strconv"
+	"wallet-wise/models"
+	"wallet-wise/shared"
 
-	"github.com/volatiletech/sqlboiler/boil"
-	_ "modernc.org/sqlite"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/volatiletech/null/v8"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
-func main() {
+var router = gin.Default()
+var ctx = context.Background()
 
-	// Open db
-	db, err := sql.Open("sqlite", "./db.sqlite")
+var DB *sql.DB
+
+// getExpense
+func getExpense(c *gin.Context) {
+	id, _ := strconv.ParseInt(c.Param("id"), 6, 12)
+
+	fmt.Println(id)
+	fmt.Println(ctx)
+
+	expense, err := models.Expenses(qm.Where("expense_id = ?", 1)).One(ctx, DB)
 	if err != nil {
-		log.Fatal("Error: Cannot open sqlite3 database: ", err)
+		log.Fatal("Error: Cannot open table categories: ", err)
 	}
-	defer db.Close()
+	fmt.Println(expense)
 
-	boil.SetDB(db)
+	// expense, err := models.Expenses(qm.Where("id = ?", id)).One(ctx, db)
+	// if err != nil {
+	// 	log.Fatal("failed to get expense: %w", err)
+	// }
+	// fmt.Println(expense)
 
-	//ctx := context.Background()
-	//_, err = models.Expense().All(ctx, db)
-	//if err != nil {
-	//	log.Fatal("Error: Cannot open table categories: ", err)
-	//}
+	c.Header("Content-Type", "application/json")
 
-	routes.Run()
+	c.JSON(http.StatusOK, gin.H{
+		"message": id,
+	})
+}
+
+// postExpense adds a charge from JSON received in the request body.
+func postExpense(c *gin.Context) {
+	var newExpense shared.Expense
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newExpense); err != nil {
+		return
+	}
+
+	fmt.Println("blah")
+
+	var expense models.Expense
+
+	expense.ExpenseID = null.Int64From(newExpense.ID)
+	expense.ExpenseAmount = newExpense.Amount
+	expense.ExpenseCategoryID = null.Int64From(newExpense.CategoryID)
+	expense.ExpenseDescription = newExpense.Description
+	expense.ExpenseDate = null.Int64From(newExpense.Date)
+
+	// Fill in the fields of the Expense struct here
+	// For example:
+	// Amount:      100.50,
+	// Description: "Office supplies",
+	// Date:        time.Now(),
+
+	err := expense.InsertG(ctx, boil.Infer())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Add the new album to the slice.
+	//albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newExpense)
+}
+
+func addExpenseRoutes(rg *gin.RouterGroup) {
+	expense := rg.Group("/expense")
+
+	expense.GET("/:id", getExpense)
+	expense.POST("/", postExpense)
+}
+
+func addPingRoutes(rg *gin.RouterGroup) {
+	ping := rg.Group("/ping")
+
+	ping.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "pong")
+	})
+}
+
+func openDB(dbPath string) (*sql.DB, error) {
+	// Open the SQLite database
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	fmt.Println("Successfully connected to the SQLite database")
+
+	return db, nil
+}
+
+func main() {
+	dbPath := "./db.sqlite"
+	var err error
+
+	DB, err = openDB(dbPath)
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+	defer DB.Close() // Ensure the database connection is closed when the program exits
+	boil.SetDB(DB)
+
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./dist", true)))
+	api := router.Group("/api")
+	addExpenseRoutes(api)
+	v2 := router.Group("/v2")
+	addPingRoutes(v2)
+
+	router.Run(":3000")
 }
